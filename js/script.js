63f31afccaeb464a602d01c77b72fa673d90a042
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-11-24 15:14:18 -0500

    display a modal when user clicks on an employee profile

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const fetchData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     displayEmployee(data.results);
+    createModal();
   } catch (error) {}
 };
 
@@ -19,8 +20,8 @@ const displayEmployee = (employees) => {
 
         <div class="card-info-container">
           <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-          <p class="card-text">${employee.email}</p>
-          <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+          <p class="card-text card-email">${employee.email}</p>
+          <p class="card-text card-city cap">${employee.location.city}, ${employee.location.state}</p>
         </div>
       </div>
     `;
@@ -28,15 +29,38 @@ const displayEmployee = (employees) => {
   });
 };
 
-fetchData("https://randomuser.me/api/?results=12");
+function createModal() {
+  const cards = document.querySelectorAll(".card");
+  for (const card of cards) {
+    card.addEventListener("click", (e) => {
+      console.log("modal");
+      const imageContainer = card.querySelector(".card-img-container");
+      const infoContainer = card.querySelector(".card-info-container");
+      const modal = `
+      <div class="modal-container">
+        <div class="modal">
+          <img class="modal-img" src="${
+            imageContainer.querySelector("img").src
+          }" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${
+            infoContainer.querySelector("h3.card-name").textContent
+          }</h3>
+          <p class="modal-text">${
+            infoContainer.querySelector("p.card-email").textContent
+          }</p>
+          <p class="modal-text cap">${
+            infoContainer.querySelector("p.card-city").textContent
+          }</p>
+          <hr>
+          <p class="modal-text">(555) 555-5555</p>
+          <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+          <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>
+    </div>
+      `;
+      gallery.insertAdjacentHTML("afterend", modal);
+    });
+  }
+}
 
-// <div class="card">
-// <div class="card-img-container">
-//     <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-// </div>
-// <div class="card-info-container">
-//     <h3 id="name" class="card-name cap">first last</h3>
-//     <p class="card-text">email</p>
-//     <p class="card-text cap">city, state</p>
-// </div>
-// </div>
+fetchData("https://randomuser.me/api/?results=12");

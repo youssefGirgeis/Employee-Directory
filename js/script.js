1d376d21e55c7abe00a49c8f39c48e3107e7f96e
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-11-23 23:01:12 -0500

    Display 12 random employees

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const gallery = document.getElementById("gallery");
+
 const fetchData = async (url) => {
   try {
     const response = await fetch(url);
@@ -6,4 +8,35 @@ const fetchData = async (url) => {
   } catch (error) {}
 };
 
+const displayEmployee = (employees) => {
+  console.log(employees);
+  employees.forEach((employee) => {
+    const employeeHTML = `
+      <div class="card">
+        <div class="card-img-container">
+          <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+        </div>
+
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+          <p class="card-text">${employee.email}</p>
+          <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+        </div>
+      </div>
+    `;
+    gallery.insertAdjacentHTML("beforeend", employeeHTML);
+  });
+};
+
 fetchData("https://randomuser.me/api/?results=12");
+
+// <div class="card">
+// <div class="card-img-container">
+//     <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+// </div>
+// <div class="card-info-container">
+//     <h3 id="name" class="card-name cap">first last</h3>
+//     <p class="card-text">email</p>
+//     <p class="card-text cap">city, state</p>
+// </div>
+// </div>
